ed8b40b2a84b198366e1d324369b61d04c0e02a4
Changes <nirmalyaiitk@gmail.com>
2023-07-07 19:22:37 +0530

    Project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { Navbar, Footer } from "@/components";
+import "./global.css";
+
+export const metadata = {
+  title: "Flexibble",
+  description: "Showcase and discover remarable developer projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/index.jsx
@@ -0,0 +1,4 @@
+import Navbar from "./Nanbar";
+import Footer from "./Footer";
+
+export { Navbar, Footer };
